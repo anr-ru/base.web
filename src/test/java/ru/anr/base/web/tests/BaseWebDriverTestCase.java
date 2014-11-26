f4d59605d87268213b6ecee0e8b21759eb6ac96c
Alexey Romanchuk <alexey.p.romanchuk@gmail.com>
2014-11-26 17:10:20 +0500

    Created a base class for Selenide-based testing

---
+++ src/test/java/ru/anr/base/web/tests/BaseWebDriverTestCase.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package ru.anr.base.web.tests;
+
+import org.junit.Ignore;
+import org.junit.Rule;
+
+import ru.anr.base.facade.tests.AbstractGlassfishWebTestCase;
+
+import com.codeborne.selenide.junit.ScreenShooter;
+
+/**
+ * Base test case class for Using Selenide framework in tests.
+ *
+ *
+ * @author Alexey Romanchuk
+ * @created Nov 26, 2014
+ *
+ */
+@Ignore
+public class BaseWebDriverTestCase extends AbstractGlassfishWebTestCase {
+
+    /**
+     * JUnit test rule for browser screenshots
+     */
+    @Rule
+    public ScreenShooter shots = ScreenShooter.failedTests().to("target/screenshots");
+
+}
