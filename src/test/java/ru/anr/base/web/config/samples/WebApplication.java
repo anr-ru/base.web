788bf2dd2e198593bd23cb783e10d473676aa24c
Alexey Romanchuk <alexey.p.romanchuk@gmail.com>
2023-12-16 22:30:18 +0500

    Migrate to 'yml' instead of properties

--- src/test/java/ru/anr/base/web/config/samples/WebApplication.java
+++ src/test/java/ru/anr/base/web/config/samples/WebApplication.java
@@ -4,9 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.*;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import ru.anr.base.services.BaseService;
@@ -29,6 +28,10 @@ import ru.anr.base.web.controllers.AbstractApplication;
 @EnableWebMvc
 @EnableWebSecurity
 //@EnableZuulProxy  // Must be enabled when doing local development in the Angular app
+@PropertySources({
+        @PropertySource(value = "classpath:application.yml", ignoreResourceNotFound = true),
+        @PropertySource(value = "file:application.yml", ignoreResourceNotFound = true)
+})
 public class WebApplication extends AbstractApplication {
 
     /**
